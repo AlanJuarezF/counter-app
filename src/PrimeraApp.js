dd82e7adcfdc3b05e5bd218b30c4d8fdf99681ff
Alan Juarez <alanwlfx@gmail.com>
2022-02-22 13:20:29 -0600

    ADD: Impresion de variables

--- src/PrimeraApp.js
+++ src/PrimeraApp.js
@@ -1,15 +1,16 @@
 import React from 'react';
-
-
 //import React, { Fragment } from 'react';
+
 
 
 const PrimeraApp = () =>{
 
+    const saludo = 'Hola mundo Const'
 
     return (
         <>
-        <h1>Hola Mundo</h1>
+        <h1>{saludo}</h1>
+        {/* <pre> {JSON.stringify(saludo, null, 3)} </pre> */}
         <p>Mi PrimeraApp</p>
         </>
     );
