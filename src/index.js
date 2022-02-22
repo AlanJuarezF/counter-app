a73fbb0ff35935c4937358d310639dbee5184a92
Alan Juarez <alanwlfx@gmail.com>
2022-02-22 14:18:47 -0600

    ADD: ComponentCounterApp

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import  ReactDOM  from 'react-dom';
 import PrimeraApp from './PrimeraApp';
+import CounterApp from './CounterApp';
+
 import './index.css';
 
 const divRoot = document.querySelector('#app');
 
-ReactDOM.render(<PrimeraApp saludo="Hola" />, divRoot);
+ReactDOM.render(<CounterApp value={12} />, divRoot);
